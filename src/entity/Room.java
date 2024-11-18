b3b05bafc92e7c3e1bf9ae7a40faa610abfaa134
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-11-18 12:32:42 -0500

    Update Room.java

--- src/entity/Room.java
+++ src/entity/Room.java
@@ -7,14 +7,12 @@ public class Room {
         this.width = width;
         this.height = height;
     }
-    // Check if this room overlaps with another room
     public boolean overlaps(Room other){
         return (this.x < other.x + other.width &&
                 this.x + this.width > other.x &&
                 this.y < other.y + other.height &&
                 this.y + this.height > other.y);
     }
-    // Get center coordinates of the room
     public int getCenterX() {
         return x + width / 2;
     }
