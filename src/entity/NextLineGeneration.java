f03f56d5a4c5de6eb8945fc2c482e76f362ce1ff
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-11-27 12:23:40 -0500

    Update NextLineGeneration.java

--- src/entity/NextLineGeneration.java
+++ src/entity/NextLineGeneration.java
@@ -23,36 +23,27 @@ public class NextLineGeneration {
                     int tileType = secureRandom.nextInt(100);
                     int totalTile;
 
-                    // Build totalTileLocations
                     List<Integer> totalTileLocations = new ArrayList<>(nextTileLocations);
                     totalTileLocations.addAll(GamePanel.tileLocations);
 
-                    // Index of the tile we are generating in totalTileLocations
                     int indexInTotalTileLocations = nextTileLocations.size();
 
                     if (tileType <= 20) {
-                        // Try to place a stair tile
-                        int range = 10; // Specify the range to check
+                        int range = 10;
                         if (RangeChecker.isInRange(totalTileLocations, gridWidth, indexInTotalTileLocations, range, 2)) {
-                            // Stair tile nearby, place a ground tile instead
                             totalTile = Integer.parseInt(Integer.toString(secureRandom.nextInt(5) + 1) + "0");
                         } else {
-                            // No stair tile nearby, place a stair tile
                             totalTile = Integer.parseInt("1" + "2");
                         }
                     } else {
-                        // Place a ground tile
                         totalTile = Integer.parseInt(Integer.toString(secureRandom.nextInt(5) + 1) + "0");
                     }
                     nextTileLocations.add(totalTile);
                 }
             }
         }
-
-        // Add new tiles to GamePanel.tileLocations
-        //int gridWidth = gp.screenWidth / gp.tileSize;
         for (int i = 0; i < gridWidth; i++) {
-            GamePanel.tileLocations.addFirst(nextTileLocations.remove(0));
+            GamePanel.tileLocations.addFirst(nextTileLocations.removeFirst());
         }
 
         // Remove the oldest tiles
