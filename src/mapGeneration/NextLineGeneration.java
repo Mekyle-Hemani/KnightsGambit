6a6a214f5599dc337cdd7652ba11fa86567b619a
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-12-10 13:15:21 -0500

    Update NextLineGeneration.java

--- src/mapGeneration/NextLineGeneration.java
+++ src/mapGeneration/NextLineGeneration.java
@@ -29,77 +29,72 @@ public class NextLineGeneration {
 
                     int indexInTotalTileLocations = nextTileLocations.size();
 
-                    if (true) {
-                        int range = secureRandom.nextInt(1,6);
-                        int compareRange = range;
+                    int range = secureRandom.nextInt(1, 6);
+                    int compareRange = range;
 
-                        if (compareRange % 2 == 1) {
-                            compareRange -= 1;
-                        }
-                        compareRange = compareRange / 2;
+                    if (compareRange % 2 == 1) {
+                        compareRange -= 1;
+                    }
+                    compareRange = compareRange / 2;
 
-                        compareRange += 5;
+                    compareRange += 5;
 
-                        if (RangeChecker.isInRange(totalTileLocations, gridWidth, indexInTotalTileLocations, compareRange, 2) || RangeChecker.isInRange(totalTileLocations, gridWidth, indexInTotalTileLocations, compareRange, 1)) {
-                            totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
-                            nextTileLocations.add(totalTile);
-                        } else {
-                            //Check if the iteration is on the edge of screen
-                            totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
-                            nextTileLocations.add(totalTile);
-
-                            if (range % 2 == 1) {
-                                range -= 1;
-                            }
-                            range = range / 2;
-
-                            //Add position left
-                            for (int j = 0; j < range+1; j++){
-                                totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
-                                nextTileLocations.add(totalTile);
-                            }
-                            int wallType = Integer.parseInt(Integer.toString(secureRandom.nextInt(5)+1) + 1);
-
-                            //Add bottom door
-                            int middleBottomWall = nextTileLocations.size()-(((range+1)*gridWidth)+2+range);
-                            nextTileLocations.set(middleBottomWall, 12);
-
-                            for (int n = 0; n<(range+1)*gridWidth; n++){
-                                totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
-                                nextTileLocations.add(totalTile);
-                            }
-
-                            //Bottom wall, left wall, right wall
-                            int topWallHeight = 0;
-                            for (int k = 0; k<range+1; k++){
-                                nextTileLocations.set(middleBottomWall+k+1, wallType);
-                                nextTileLocations.set(middleBottomWall-k-1, wallType);
-                                if (k==range){
-                                    for (int m = 1; m<range*2+3; m++){
-                                        nextTileLocations.set(middleBottomWall+k+1+(gridWidth*m), wallType);
-                                        nextTileLocations.set(middleBottomWall-k-1+(gridWidth*m), wallType);
-                                        topWallHeight = middleBottomWall+k+1+(gridWidth*m);
-                                    }
-                                }
-                            }
-                            int randomValue = secureRandom.nextInt(2);
-                            for (int n = 1; n<range*2+3; n++) {
-                                if (n == range+1) {
-                                    if (randomValue == 0) {
-                                        nextTileLocations.set(topWallHeight-n, 12);
-                                    } else {
-                                        nextTileLocations.set(topWallHeight-n, wallType);
-                                    }
-                                } else {
-                                    nextTileLocations.set(topWallHeight - n, wallType);
-                                }
-                            }
-
-                            i+=range+1;
-                        }
-                    } else {
+                    if (RangeChecker.isInRange(totalTileLocations, gridWidth, indexInTotalTileLocations, compareRange, 2) || RangeChecker.isInRange(totalTileLocations, gridWidth, indexInTotalTileLocations, compareRange, 1)) {
                         totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
                         nextTileLocations.add(totalTile);
+                    } else {
+                        //Check if the iteration is on the edge of screen
+                        totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
+                        nextTileLocations.add(totalTile);
+
+                        if (range % 2 == 1) {
+                            range -= 1;
+                        }
+                        range = range / 2;
+
+                        //Add position left
+                        for (int j = 0; j < range+1; j++){
+                            totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
+                            nextTileLocations.add(totalTile);
+                        }
+                        int wallType = Integer.parseInt(Integer.toString(secureRandom.nextInt(5)+1) + 1);
+
+                        //Add bottom door
+                        int middleBottomWall = nextTileLocations.size()-(((range+1)*gridWidth)+2+range);
+                        nextTileLocations.set(middleBottomWall, 12);
+
+                        for (int n = 0; n<(range+1)*gridWidth; n++){
+                            totalTile = Integer.parseInt(secureRandom.nextInt(5) + 1 + "0");
+                            nextTileLocations.add(totalTile);
+                        }
+
+                        //Bottom wall, left wall, right wall
+                        int topWallHeight = 0;
+                        for (int k = 0; k<range+1; k++){
+                            nextTileLocations.set(middleBottomWall+k+1, wallType);
+                            nextTileLocations.set(middleBottomWall-k-1, wallType);
+                            if (k==range){
+                                for (int m = 1; m<range*2+3; m++){
+                                    nextTileLocations.set(middleBottomWall+k+1+(gridWidth*m), wallType);
+                                    nextTileLocations.set(middleBottomWall-k-1+(gridWidth*m), wallType);
+                                    topWallHeight = middleBottomWall+k+1+(gridWidth*m);
+                                }
+                            }
+                        }
+                        int randomValue = secureRandom.nextInt(2);
+                        for (int n = 1; n<range*2+3; n++) {
+                            if (n == range+1) {
+                                if (randomValue == 0) {
+                                    nextTileLocations.set(topWallHeight-n, 12);
+                                } else {
+                                    nextTileLocations.set(topWallHeight-n, wallType);
+                                }
+                            } else {
+                                nextTileLocations.set(topWallHeight - n, wallType);
+                            }
+                        }
+
+                        i+=range+1;
                     }
                 }
             }
