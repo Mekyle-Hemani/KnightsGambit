ca802be48c2ae0bac82c1586c8e70619cd713607
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2025-01-13 15:35:04 -0500

    Update midGameSave.java

--- src/saveFunction/midGameSave.java
+++ src/saveFunction/midGameSave.java
@@ -1,22 +1,26 @@
 package saveFunction;
 
 import main.GamePanel;
+import mapGeneration.NextLineGeneration;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class midGameSave {
     public static void save() throws IOException {
-        java.util.List<String> savingValues = new ArrayList<>();
-        savingValues.add(Integer.toString(GamePanel.spacesCrossed));
-        save.save(savingValues, "save.txt");
+        java.util.List<String> savingValuesTiles = new ArrayList<>();
+        java.util.List<String> savingValuesCrossed = new ArrayList<>();
 
+        List<Integer> totalTileLocations = new ArrayList<>(NextLineGeneration.nextTileLocations);
+        totalTileLocations.addAll(GamePanel.tileLocations);
+        savingValuesTiles.add(totalTileLocations.toString());
+        save.save((savingValuesTiles), "tiles.txt");
 
-        //java.util.List<String> savingValues = new ArrayList<>();
-        //savingValues.add("1");
-        //save.save(haha, "save.txt");
-        //System.out.println(save.load("save.txt"));
+        savingValuesCrossed.add(String.valueOf(GamePanel.spacesCrossed));
+        save.save((savingValuesCrossed), "spaces.txt");
 
-        System.out.println(save.load("save.txt"));
+        System.out.println(save.load("tiles.txt"));
+        System.out.println(save.load("spaces.txt"));
     }
 }
